fa5f496cd3d907d445cf3720964b388e7023ecb2
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2018-12-12 10:36:47 -0800

    WIP -- DO NOT MERGE UNTIL CLEANED

--- acceptance/tests/resource/exec/should_run_command_in_cwd.rb
+++ acceptance/tests/resource/exec/should_run_command_in_cwd.rb
@@ -101,70 +101,78 @@ test_name "The Exec resource should run commands in the specified cwd" do
       end
     end
 
-    # tmpdir_noaccess = agent.tmpdir("mock_dir")
-    # create_remote_file(agent, File.join(tmpdir_noaccess, 'noaccess.txt'), 'foobar')
-    # username = "pl#{rand(999999).to_i}"
+    tmpdir_noaccess = agent.tmpdir("mock_dir")
+    create_remote_file(agent, File.join(tmpdir_noaccess, 'noaccess.txt'), 'foobar')
+    username = "pl#{rand(999999).to_i}"
 
-    # step "Setup user for 'no access' test" do
-    #   agent.user_present(username)
-    #   if agent.platform =~ /solaris/
-    #     # for some reason applications of 'user_present' on solaris 10 don't manage the homedir correctly, so just
-    #     # force a puppet apply to manage the user
-    #     on agent, puppet_resource('user', username, "ensure=present managehome=true home=/export/home/#{username}")
-    #     # we need to create the user directory ourselves in order for solaris users to successfully login
-    #     on(agent, "mkdir /export/home/#{username} && chown -R #{username} /export/home/#{username}")
-    #   elsif agent.platform =~ /osx/
-    #     # we need to create the user directory ourselves in order for macos users to successfully login
-    #     on(agent, "mkdir /Users/#{username} && chown -R #{username}:80 /Users/#{username}")
-    #   end
-    # end
+    step "Setup user for 'no access' test" do
+      agent.user_present(username)
+      if agent.platform =~ /solaris/
+        # for some reason applications of 'user_present' on solaris 10 don't manage the homedir correctly, so just
+        # force a puppet apply to manage the user
+        on agent, puppet_resource('user', username, "ensure=present managehome=true home=/export/home/#{username}")
+        # we need to create the user directory ourselves in order for solaris users to successfully login
+        on(agent, "mkdir /export/home/#{username} && chown -R #{username} /export/home/#{username}")
+      elsif agent.platform =~ /osx/
+        # we need to create the user directory ourselves in order for macos users to successfully login
+        on(agent, "mkdir /Users/#{username} && chown -R #{username}:80 /Users/#{username}")
+      end
+    end
 
-    # tmpdir_noaccess = agent.tmpdir("mock_noaccess")
-    # create_remote_file(agent, File.join(tmpdir_noaccess, 'noaccess.txt'), 'foobar')
+    tmpdir_noaccess = agent.tmpdir("mock_noaccess")
+    create_remote_file(agent, File.join(tmpdir_noaccess, 'noaccess.txt'), 'foobar')
 
-    # step "Setup restricted access directory for 'no access' test" do
-    #   if agent.platform =~ /windows/
-    #     deny_administrator_access_to(agent, tmpdir_noaccess)
-    #     deny_administrator_access_to(agent, File.join(tmpdir_noaccess, 'noaccess.txt'))
-    #   else
-    #     if agent.platform =~ /osx/
-    #       # This is a little nuts, but on MacOS the tmpdir returned from agent.tmpdir is located in
-    #       # a directory that users other than root can't even access, i.e. other users won't have access
-    #       # to either the noaccess dir itself (which we want) _or the tmpdir root it's located in_. This is
-    #       # a problem since it will look to puppet like the noacceess dir doesn't exist at all, and so we
-    #       # can't count on any reliaable failure since we want a return indicating no access, not a missing directory.
-    #       #
-    #       # To get around this for MacOS platforms we simply use the new user's homedir as the 'tmpdir' and
-    #       # put the noaccess dir there.
-    #       on(agent, "mkdir /Users/#{username}/noaccess_test && cp #{tmpdir_noaccess}/noaccess.txt /Users/#{username}/noaccess_test && chmod -R 600 /Users/#{username}/noaccess_test")
-    #       tmpdir_noaccess = "/Users/#{username}/noaccess_test"
-    #     end
-    #     # remove permissions for all other users other than root, which should force puppet to fail when running as another user
-    #     on(agent, "chmod -R 600 #{tmpdir_noaccess}")
-    #   end
-    # end
+    step "Setup restricted access directory for 'no access' test" do
+      if agent.platform =~ /windows/
+        deny_administrator_access_to(agent, tmpdir_noaccess)
+        deny_administrator_access_to(agent, File.join(tmpdir_noaccess, 'noaccess.txt'))
+      else
+        if agent.platform =~ /osx/
+          # This is a little nuts, but on MacOS the tmpdir returned from agent.tmpdir is located in
+          # a directory that users other than root can't even access, i.e. other users won't have access
+          # to either the noaccess dir itself (which we want) _or the tmpdir root it's located in_. This is
+          # a problem since it will look to puppet like the noacceess dir doesn't exist at all, and so we
+          # can't count on any reliaable failure since we want a return indicating no access, not a missing directory.
+          #
+          # To get around this for MacOS platforms we simply use the new user's homedir as the 'tmpdir' and
+          # put the noaccess dir there.
+          on(agent, "mkdir /Users/#{username}/noaccess_test && cp #{tmpdir_noaccess}/noaccess.txt /Users/#{username}/noaccess_test && chmod -R 600 /Users/#{username}/noaccess_test")
+          tmpdir_noaccess = "/Users/#{username}/noaccess_test"
+        end
+        # remove permissions for all other users other than root, which should force puppet to fail when running as another user
+        on(agent, "chmod -R 600 #{tmpdir_noaccess}")
+      end
+    end
 
-    # step "Errors if the user does not have access to the specified CWD" do
-    #   manifest_path = agent.tmpfile('apply_manifest.pp')
-    #   create_remote_file(agent, manifest_path, exec_resource_manifest("#{cat} noaccess.txt", {:cwd => tmpdir_noaccess, :path => path}))
-    #   if agent.platform =~ /windows/
-    #     on(agent, command["puppet apply #{manifest_path} --detailed-exitcodes"], :acceptable_exit_codes => [4, 6])
-    #   elsif agent.platform =~ /osx/
-    #     # on MacOS we need to copy the manifest to run to the user's home dir and give the user ownership. otherwise puppet won't run on it.
-    #     on(agent, "cp #{manifest_path} /Users/#{username}/noaccess_manifest.pp && chown #{username}:80 /Users/#{username}/noaccess_manifest.pp")
-    #     on(agent, command["su - #{username} -c \"/opt/puppetlabs/bin/puppet apply /Users/#{username}/noaccess_manifest.pp --detailed-exitcodes\""], :acceptable_exit_codes => [4, 6])
-    #   else
-    #     on(agent, "chown #{username} #{manifest_path}")
-    #     if agent.platform =~ /solaris|aix/
-    #       on(agent, command["su - #{username} -c \"/opt/puppetlabs/bin/puppet apply #{manifest_path} --detailed-exitcodes\""], :acceptable_exit_codes => [4])
-    #     else
-    #       on(agent, command["su #{username} -c \"/opt/puppetlabs/bin/puppet apply #{manifest_path} --detailed-exitcodes\""], :acceptable_exit_codes => [4, 6])
-    #     end
-    #   end
-    # end
+    step "Errors if the user does not have access to the specified CWD" do
+      manifest_path = agent.tmpfile('apply_manifest.pp')
+      create_remote_file(agent, manifest_path, exec_resource_manifest("#{cat} noaccess.txt", {:cwd => tmpdir_noaccess, :path => path}))
+      if agent.platform =~ /windows/
+        on(agent, "puppet apply #{manifest_path} --detailed-exitcodes") do |result|
+          assert_equal(4, result.exit_code, "Exec manifest still executed inside restricted directory")
+        end
+      elsif agent.platform =~ /osx/
+        # on MacOS we need to copy the manifest to run to the user's home dir and give the user ownership. otherwise puppet won't run on it.
+        on(agent, "cp #{manifest_path} /Users/#{username}/noaccess_manifest.pp && chown #{username}:80 /Users/#{username}/noaccess_manifest.pp")
+        on(agent, "su - #{username} -c \"/opt/puppetlabs/bin/puppet apply /Users/#{username}/noaccess_manifest.pp --detailed-exitcodes\"") do |result|
+          assert_equal(4, result.exit_code, "Exec manifest still executed inside restricted directory")
+        end
+      else
+        on(agent, "chown #{username} #{manifest_path}")
+        if agent.platform =~ /solaris|aix/
+          on(agent, "su - #{username} -c \"/opt/puppetlabs/bin/puppet apply #{manifest_path} --detailed-exitcodes\"") do |result|
+            assert_equal(4, result.exit_code, "Exec manifest still executed inside restricted directory")
+          end
+        else
+          on(agent, "su #{username} -c \"/opt/puppetlabs/bin/puppet apply #{manifest_path} --detailed-exitcodes\"") do |result|
+            assert_equal(4, result.exit_code, "Exec manifest still executed inside restricted directory")
+          end
+        end
+      end
+    end
 
-    # step "remove test user once testing with it is done" do
-    #   agent.user_absent(username)
-    # end
+    step "remove test user once testing with it is done" do
+      agent.user_absent(username)
+    end
   end
 end
